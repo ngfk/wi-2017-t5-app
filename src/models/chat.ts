5c3716fcdf354de701d9341abb628547d1dbd506
Rick Koster <ngf.koster@outlook.com>
2017-11-26 09:20:36 +0100

    feat(core): add possibility to have images in chat

--- src/models/chat.ts
+++ src/models/chat.ts
@@ -1,7 +1,27 @@
-export interface Chat extends Array<ChatEntry> {}
+export interface ChatEntryText {
+    readonly text: string;
+}
 
-export interface ChatEntry {
+export interface ChatEntryImage {
+    readonly title: string;
+    readonly image: string;
+}
+
+export enum ChatType {
+    Text = 'text',
+    Image = 'image'
+}
+
+export interface ChatTypeMap {
+    text: ChatEntryText;
+    image: ChatEntryImage;
+}
+
+export interface ChatEntry<T extends ChatType = ChatType.Text> {
     readonly timestamp: Date;
     readonly from?: string;
-    readonly content: string;
+    readonly type: T;
+    readonly content: ChatTypeMap[T];
 }
+
+export interface Chat extends Array<ChatEntry<ChatType>> {}
--- src/pages/chat/chat.page.ts
+++ src/pages/chat/chat.page.ts
@@ -18,7 +18,8 @@ import {
 } from 'ionic-angular';
 import { Subscription } from 'rxjs/Subscription';
 
-import { Chat, ChatEntry } from '../../models/chat';
+import { Chat, ChatEntry, ChatType } from '../../models/chat';
+import { chat } from './temp';
 
 @IonicPage()
 @Component({
@@ -30,7 +31,7 @@ export class ChatPage implements OnInit, OnDestroy {
     @ViewChild(Header) public headerField: Header;
     @ViewChild(Content) public contentField: Content;
     @ViewChild('input') public inputField: TextInput;
-    public chat: Chat;
+    public chat: Chat = chat;
 
     private subscriptions: Subscription[] = [];
 
@@ -43,7 +44,6 @@ export class ChatPage implements OnInit, OnDestroy {
     ) {}
 
     public ngOnInit(): void {
-        this.loadFakeChat();
         this.subscriptions = [
             this.keyboard.onKeyboardShow().subscribe(this.onKeyboardShow),
             this.keyboard.onKeyboardHide().subscribe(this.onKeyboardHide)
@@ -60,13 +60,14 @@ export class ChatPage implements OnInit, OnDestroy {
         setTimeout(() => this.contentField.scrollToBottom(0), 0);
     }
 
-    public send(input: string): void {
-        input = input.trim();
+    public sendText(text: string): void {
+        text = text.trim();
 
-        if (input) {
+        if (text) {
             const newEntry: ChatEntry = {
-                content: input,
-                timestamp: new Date()
+                timestamp: new Date(),
+                type: ChatType.Text,
+                content: { text }
             };
             this.chat.push(newEntry);
         }
@@ -105,36 +106,4 @@ export class ChatPage implements OnInit, OnDestroy {
             this.cd.detectChanges();
         }
     };
-
-    private loadFakeChat(): void {
-        this.chat = [
-            {
-                timestamp: new Date(),
-                from: 'Watson',
-                content: 'Hello World!'
-            },
-            {
-                timestamp: new Date(),
-                content: 'Hey!'
-            },
-            {
-                timestamp: new Date(),
-                from: 'Watson',
-                content:
-                    // tslint:disable-next-line max-line-length
-                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer leo purus, molestie quis nisi eu, sagittis eleifend nibh. In hac habitasse platea dictumst. Nam eu mi nec enim placerat sodales. Sed commodo placerat ligula, sit amet efficitur nisl posuere nec. Duis maximus, nibh quis volutpat blandit, metus sem fermentum neque, in volutpat orci est at erat. Sed porttitor lorem nec est ultricies, eu vulputate est mattis. Cras a quam faucibus dui imperdiet laoreet. Cras eget blandit purus, volutpat auctor augue. Curabitur convallis nibh sed nibh egestas rhoncus. Aliquam posuere nisl justo, id dictum nisi vulputate et. Nulla ac sagittis felis, vitae eleifend leo. Suspendisse metus velit, dictum non gravida sit amet, rhoncus imperdiet odio. Quisque consectetur tellus non mi finibus faucibus. Mauris tempus interdum nisi eget tempor.\nDonec malesuada semper augue id eleifend. Morbi tempus interdum felis at efficitur. Suspendisse sed est dui. Duis interdum, elit sed euismod posuere, dui arcu malesuada dolor, vel placerat tortor lacus nec erat. Sed fringilla ante eget dolor gravida, egestas eleifend dolor semper. Praesent id convallis enim, in venenatis sem. Curabitur facilisis fermentum dolor. Suspendisse gravida mollis lectus nec volutpat. Vestibulum non ullamcorper purus, in mollis urna.\nIn mollis velit non consectetur accumsan. Duis vel sem egestas, congue lectus vel, dapibus libero. Ut vitae maximus enim, nec mollis elit. Vivamus in neque cursus, aliquam eros lobortis, hendrerit sapien. Donec vehicula eu lectus nec vehicula. Suspendisse venenatis cursus quam ut tristique. Nulla in mollis nulla. Integer pharetra ullamcorper nibh, non varius massa porta vitae. Morbi et neque aliquam, suscipit lectus vitae, pellentesque quam. Maecenas vitae ligula erat. Aenean sed viverra libero, sed tempus tellus. Phasellus tristique non nulla et bibendum. Etiam ut est sem. In efficitur faucibus tellus, non auctor arcu vestibulum quis.\nDonec sit amet vehicula dui. Proin non ligula sed nisi suscipit varius ac nec mi. Mauris faucibus hendrerit sem, in tristique tortor volutpat sit amet. Maecenas vel varius lectus, vel egestas odio. Nulla facilisi. Suspendisse molestie tempus purus, non tempus leo aliquam at. Integer eget ultrices massa. Sed egestas mauris ipsum, eget porttitor lacus dapibus at. Nulla nunc sem, fermentum ac pretium vitae, vestibulum ac erat.\nPhasellus nibh mauris, semper eget lacinia non, vehicula ac quam. Nam quis enim sapien. Sed et varius lectus. Phasellus vel vulputate lorem. Suspendisse vulputate convallis tortor. Curabitur sollicitudin eget lorem sit amet tempor. Cras et ipsum porta orci ullamcorper pellentesque a sed arcu. Mauris accumsan, ligula in feugiat dictum, nunc nulla rhoncus enim, ut mollis odio erat a orci. Morbi sed hendrerit lacus.'
-            },
-            {
-                timestamp: new Date(),
-                content: '...'
-            },
-            {
-                timestamp: new Date(),
-                from: 'Watson',
-                content:
-                    // tslint:disable-next-line max-line-length
-                    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ab numquam porro. Odit harum totam voluptas laudantium modi ea laborum, omnis velit in, soluta incidunt architecto nobis quo magni dolore!'
-            }
-        ];
-    }
 }
